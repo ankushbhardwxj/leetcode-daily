0c9582f3f745973b1102dbc1ce947ff0743abfa3
Ankush Bhardwaj <40923324+ankushbhardwxj@users.noreply.github.com>
2023-11-20 10:17:09 +0530

    feat: add port and health

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import getMailingList from "./services/get-mailing-list";
 import getTodayQuestions from "./services/get-today-questions";
 import getTodayRevisionQuestions from "./services/get-today-revision-questions";
 import cron from "node-cron";
+import http from "http";
 
 cron.schedule("0 5 * * *", async () => {
   const mailingList = getMailingList();
@@ -10,3 +11,13 @@ cron.schedule("0 5 * * *", async () => {
   const revisionQuestions = getTodayRevisionQuestions();
   await sendResendEmail(mailingList, questions, revisionQuestions);
 });
+
+const server = http.createServer((req, res) =>
+  req.url === "/health"
+    ? res.end(JSON.stringify({ status: "ok" }))
+    : res.end("Not Found")
+);
+
+server.listen(3000, () =>
+  console.log("Server running on http://localhost:3000")
+);
